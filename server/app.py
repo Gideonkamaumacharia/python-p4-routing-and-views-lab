45f5c937486344e729862b3c32552c9cd963a8d9
Gideon Macharia <kamaugedion95@gmail.com>
2024-06-24 17:32:06 +0300

    commit

--- server/app.py
+++ server/app.py
@@ -16,9 +16,13 @@ def print_string(param):
 @app.route('/count/<int:param>')
 def count_route(param):
     results = ''
-    for num in range(1, param + 1):
+    for num in range(0, param):
         results += str(num) + '\n'
     return results
+
+@app.route('/math/<num1>/<opertaion>/<num2>')
+def math_route(num1,operation,num2):
+    
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
