259bbc0829659919a15ca0bb08635315811acc74
Gideon Macharia <kamaugedion95@gmail.com>
2024-06-24 17:14:59 +0300

    commit changes

--- server/app.py
+++ server/app.py
@@ -4,5 +4,21 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index_route():
+    return'<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:param>')
+def print_string(param):
+    print(param, end='\n')
+    return param
+
+@app.route('/count/<int:param>')
+def count_route(param):
+    results = ''
+    for num in range(1, param + 1):
+        results += str(num) + '\n'
+    return results
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
